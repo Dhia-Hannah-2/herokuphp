8e51680cf660bb939238dc18e1fac777e1ae120e
Dhia-Hannah-2 <20031826@rp.edu.sg>
2022-07-07 12:44:48 +0800

    initial commit

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
-<html>
+<!--fix: <html> element doesn't have a [lang] attribute-->
+<html lang="en">
 <head>
 </head>
+<!--fix: Document doesn't have a <meta name="viewport"> tag-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <body>
-  Hello World!
+    <!--fix: Document doesn't have a <title> element-->
+    <title>
+      Hello World!
+    </title>
 </body>
 </html>
