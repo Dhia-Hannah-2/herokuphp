3c06ee2073c89368189f598ed7711ee6a07cb53a
Dhia-Hannah-2 <20031826@rp.edu.sg>
2022-07-07 12:55:21 +0800

    initial commit

--- test.php
+++ test.php
@@ -1,2 +1,13 @@
-<?php
-echo "Hello World!"; 
+<!--fix: <html> element doesn't have a [lang] attribute-->
+<html lang="en">
+<head>
+</head>
+<!--fix: Document doesn't have a <meta name="viewport"> tag-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+    <!--fix: Document doesn't have a <title> element-->
+    <title color="green">
+      This is test
+    </title>
+</body>
+</html>
