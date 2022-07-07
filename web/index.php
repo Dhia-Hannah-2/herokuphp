08b88d9817cb8b2c1ef07bdfa9bcd53df1ab4ab8
Dhia-Hannah-2 <20031826@rp.edu.sg>
2022-07-07 13:17:15 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,13 +1,19 @@
+<!--fix: Page lacks the HTML doctype, thus triggering quirks-mode-->
+<!DOCTYPE html>
 <!--fix: <html> element doesn't have a [lang] attribute-->
 <html lang="en">
+
 <head>
+    <title>
+        Hello World
+    </title>
 </head>
 <!--fix: Document doesn't have a <meta name="viewport"> tag-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <body>
     <!--fix: Document doesn't have a <title> element-->
-    <title color="green">
-      Hello World green
-    </title>
+    <h1>Hello World!</h1>
 </body>
+
 </html>
